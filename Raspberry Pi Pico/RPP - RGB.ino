a8e72d6c226fd88003b9515d0faf2f87fdbb38e9
Kis Vilmos <101592804+kisvilmos27@users.noreply.github.com>
2022-11-08 21:54:46 +0100

    Update RPP - RGB.ino

--- Raspberry Pi Pico/RPP - RGB.ino
+++ Raspberry Pi Pico/RPP - RGB.ino
@@ -2,30 +2,30 @@ int r = 255;
 int g = 0;
 int b = 0;
 void setup(){
-  pinMode(16,OUTPUT); //GPIO 16
-  pinMode(17,OUTPUT); //GPIO 17
-  pinMode(18,OUTPUT); //GPIO 18
+  pinMode(16, OUTPUT); //GPIO 16, piros
+  pinMode(17, OUTPUT); //GPIO 17, zöld
+  pinMode(18, OUTPUT); //GPIO 18, kék
 }
 void loop(){
   //Piros fokozatos csökkentése, Zöld növelése
-  for(r = 255;r>0;r--){
+  for(r = 255; r > 0; r--){
     g++;
-    analogWrite(16,r);
-    analogWrite(17,g);
+    analogWrite(16,  r);
+    analogWrite(17, g);
     delay(6);
   }
   //Zöld fokozatos csökkentése, Kék növelése
-  for(g=255;g>0;g--){
+  for(g = 255; g > 0; g--){
       b++;
-    analogWrite(18,b);
-    analogWrite(17,r);
+    analogWrite(18, b);
+    analogWrite(17, r);
     delay(6);
   }
   //Kék fokozatos csökkentése, Piros növelése
-  for(b = 255;b>0;b--){
+  for(b = 255; b > 0; b--){
     r++;
-    analogWrite(18,b);
-    analogWrite(16,r);
+    analogWrite(18, b);
+    analogWrite(16, r);
     delay(6);
   }
 }
